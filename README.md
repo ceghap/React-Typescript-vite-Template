cde430b3192b5b2bfeb005daa0a563486a3f53ae
Ashraf Latif <hello@ceghap.com>
2022-09-06 23:42:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# React + Typescript + vite
+
+## includes
+
+- React 18
+- typescript
+- vite
+- vitest
+- testing library
+- react router 6
+- eslint
+- prettier
+
+### Notes
+
+Replace `HashRouter` with `BrowserRouter` if necessary
+
+## Run project
+
+- `npm run dev`
+
+## Test project
+
+- `npm test`
---
+++ src/App.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { describe, it } from 'vitest';
+import { WrappedApp, App } from './App';
+
+describe('App', () => {
+  it('Renders hello world', () => {
+    // ARRANGE
+    render(<WrappedApp />);
+    // ACT
+    // EXPECT
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Hello World'
+    );
+  });
+
+  it('Renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/pishang']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Not Found'
+    );
+  });
+});
